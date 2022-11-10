cf8d66dc7a88d67d72892529ff7ef4b9fd5b3c61
Lukas Wochner <lukas.wochner@blateral.com>
2022-11-10 15:46:07 +0100

    Updates

---
+++ precache-manifest.13f33fb20302acef7eac3e7d7fc35190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e58066c1dc8de2400d65e78bb181bc0",
+    "url": "/cookie-consent/index.html"
+  },
+  {
+    "revision": "c970864699c486282650",
+    "url": "/cookie-consent/static/css/main.8240fa95.chunk.css"
+  },
+  {
+    "revision": "b5278e5735e5d9018a86",
+    "url": "/cookie-consent/static/js/2.b932df23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cookie-consent/static/js/2.b932df23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c970864699c486282650",
+    "url": "/cookie-consent/static/js/main.5a96ff84.chunk.js"
+  },
+  {
+    "revision": "a99a9a5f6941511c18c3",
+    "url": "/cookie-consent/static/js/runtime-main.2e1db2db.js"
+  }
+]);
